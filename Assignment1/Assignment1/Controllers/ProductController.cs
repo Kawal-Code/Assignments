c78b9049bd70dd5e3a932ba409dc5aac3d027cfe
socgend44 <socgend44@iiht.tech>
2020-07-09 22:07:31 +0530

    initial commit

---
+++ Assignment1/Assignment1/Controllers/ProductController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Assignment1.Models;
+
+
+namespace Assignment1.Controllers
+{
+    public class ProductController : Controller
+    {
+        public static List<Product> list = new List<Product>()
+        {  
+          new Product() { Pid = 1, Pname = "Mac", Price = 1000, stock = 2 },
+          new Product() { Pid = 2, Pname = "Dell", Price = 1200, stock = 3 },
+          new Product() { Pid = 3, Pname = "Lenovo", Price = 1300, stock = 4 },
+        };
+       //[Route("GetAllProducts")]
+        public IActionResult Index()
+        {
+            
+            return View(list);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Product P)
+
+        {
+            list.Add(P);
+            return RedirectToAction("Index");
+
+        }
+        }
+}
---
+++ Assignment1/Assignment1/Models/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment1.Models
+{
+    public class Product
+    {   [Required]
+        public int Pid { get; set; }
+        [Required]
+        public string Pname { get; set; }
+        [Required]
+        public int Price { get; set; }
+        [Required]
+        public int stock { get; set; }
+    }
+}
